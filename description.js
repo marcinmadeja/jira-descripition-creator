199faf4102a25dea9722a1b639b9e9553628062b
Marcin Madeja <madeja.m.b@gmail.com>
2021-01-05 21:36:36 +0100

    Add options

--- description.js
+++ description.js
@@ -1,42 +1,69 @@
 (function() {
-  const getDescription = () => {
+  const getDescription = (options) => {
     let description = '';
+
+
+    if (options.includes('notes')) {
+      description += `
+        {panel:title=Notes|bgColor=#eae6ff}
+          Require role *role_name*
+
+          *Test also:*
+          * responsiveness
+          * accessibility
+          * edge cases (full length of text, no white spaces)
+          * cross browser
+        {panel}
+      `;
+    }
+
+
     description += `
-    {panel:title=Notes|bgColor=#eae6ff}
-      Require role *role_name*
-
-      *Test also:*
-      * responsiveness
-      * accessibility
-      * edge cases (full length of text, no white spaces)
-      * cross browser
-    {panel}
-
-    {panel:title=Description}
+      {panel:title=Description}
       ...
-    {panel}
-
-    {panel:title=Scenario 1}
-      *Steps:*
-
-      *Expected Results:*
-
-      *Actual Results:*
-    {panel}
-
-    {panel:title=To do before ready for review}
-      * Check responsiveness / create a ticket
-      * Check accessibility / create a ticket
-      * Test in safari / firefox / ie / edge
-      * Check edge cases (full length of text, no white spaces)
-      * Create unit, cypress, playwright tests / create ticket
-      * Update tests if necessary
-    {panel}
+      {panel}
     `;
+
+    if (options.includes('scenario')) {
+      description += `
+        {panel:title=Scenario 1}
+          *Steps:*
+
+          *Expected Results:*
+
+          *Actual Results:*
+        {panel}
+      `;
+    }
+
+    if (options.includes('regression')) {
+      description += `
+        {panel:title=Regression scenario 1}
+          Add info about potential regression related to this ticket.
+        {panel}
+      `;
+    }
+
+    if (options.includes('todo')) {
+      description += `
+        {panel:title=To do before ready for review}
+          * Check responsiveness / create a ticket
+          * Check accessibility / create a ticket
+          * Test in safari / firefox / ie / edge
+          * Check edge cases (full length of text, no white spaces)
+          * Create unit, cypress, playwright tests / create ticket
+          * Update tests if necessary
+        {panel}
+      `;
+    }
+
 
     return description;
   }
 
-  const description = document.getElementById('description');
-  description.value = getDescription();
+  chrome.storage.sync.get(['type', 'options'], function(result) {
+    const { type, options } = result;
+    const description = document.getElementById('description');
+    description.value = getDescription(options);
+  });
 })();
--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
 const addDescription = document.getElementById('add-description');
 
 addDescription.onclick = function(element) {
-  const description = getDescription();
-  // console.log('description', description);
+  const typeValue = document.querySelector('input[name="type"]:checked').value;
+  const optionsCheckboxes = document.querySelectorAll('input[name="option"]:checked');
+  const selectedOptions = [];
+
+  for (var i = 0; i < optionsCheckboxes.length; i++) {
+    selectedOptions.push(optionsCheckboxes[i].value)
+  }
+
+
+  console.log('typeValue', typeValue);
+  console.log('selectedOptions', selectedOptions);
+
+  chrome.storage.sync.set({type: typeValue});
+  chrome.storage.sync.set({options: selectedOptions});
+
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     const xxx = '1';
     chrome.tabs.executeScript(
@@ -16,12 +29,3 @@ addDescription.onclick = function(element) {
     //     {code: `document.getElementById('description').value = '${description}';`});
   });
 };
-
-const getDescription = () => {
-  let description = '';
-  description += "{panel:title=My title} Some text with a title {panel}";
-  description += "{panel:title=My title} Some text with a title 2{panel}";
-
-
-  return description;
-}
