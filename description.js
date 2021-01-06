d1218d24f0b1943c39a6378f7b98642db41fb496
Marcin Madeja <madeja.m.b@gmail.com>
2021-01-06 07:48:14 +0100

    Clean code

--- description.js
+++ description.js
@@ -1,69 +1,94 @@
 (function() {
+  const description = document.getElementById('description');
+
   const getDescription = (options) => {
     let description = '';
 
+    description += addWarning();
 
-    if (options.includes('notes')) {
-      description += `
-        {panel:title=Notes|bgColor=#eae6ff}
-          Require role *role_name*
+    if (options.includes('notes')) description += addNotes();
 
-          *Test also:*
-          * responsiveness
-          * accessibility
-          * edge cases (full length of text, no white spaces)
-          * cross browser
-        {panel}
-      `;
-    }
+    description += addDescription();
 
+    if (options.includes('scenario')) description += addScenario();
 
-    description += `
-      {panel:title=Description}
-      ...
-      {panel}
-    `;
+    if (options.includes('regression')) description += addRegression();
 
-    if (options.includes('scenario')) {
-      description += `
-        {panel:title=Scenario 1}
-          *Steps:*
-
-          *Expected Results:*
-
-          *Actual Results:*
-        {panel}
-      `;
-    }
-
-    if (options.includes('regression')) {
-      description += `
-        {panel:title=Regression scenario 1}
-          Add info about potential regression related to this ticket.
-        {panel}
-      `;
-    }
-
-    if (options.includes('todo')) {
-      description += `
-        {panel:title=To do before ready for review}
-          * Check responsiveness / create a ticket
-          * Check accessibility / create a ticket
-          * Test in safari / firefox / ie / edge
-          * Check edge cases (full length of text, no white spaces)
-          * Create unit, cypress, playwright tests / create ticket
-          * Update tests if necessary
-        {panel}
-      `;
-    }
-
+    if (options.includes('todo')) description += addTodo();
 
     return description;
   }
 
-  chrome.storage.sync.get(['type', 'options'], function(result) {
-    const { type, options } = result;
-    const description = document.getElementById('description');
-    description.value = getDescription(options);
-  });
+  const addWarning = () => {
+    return `
+      {panel:title=Notes|bgColor=#fefae6}Remove this message after a ticket is completed.{panel}
+    `;
+  };
+
+  const addNotes = () => {
+    return `
+      {panel:title=Notes|bgColor=#eae6ff}
+        Require role *role_name*
+
+        *Test also:*
+        * responsiveness
+        * accessibility
+        * edge cases (full length of text, no white spaces)
+        * cross browser
+      {panel}
+    `;
+  }
+
+  const addDescription = () => {
+    return `
+      {panel:title=Description}
+      ...
+      {panel}
+    `;
+  }
+
+  const addScenario = () => {
+    return `
+      {panel:title=Scenario 1}
+        *Steps:*
+
+        *Expected Results:*
+
+        *Actual Results:*
+      {panel}
+    `;
+  }
+
+  const addRegression = () => {
+    return `
+      {panel:title=Regression scenario 1}
+        Add info about potential regression related to this ticket.
+      {panel}
+    `;
+  }
+
+  const addTodo = () => {
+    return `
+      {panel:title=To do before ready for review}
+        * Check responsiveness / create a ticket
+        * Check accessibility / create a ticket
+        * Test in safari / firefox / ie / edge
+        * Check edge cases (full length of text, no white spaces)
+        * Create unit, cypress, playwright tests / create ticket
+        * Update tests if necessary
+        * Add no test if needed
+      {panel}
+    `;
+  }
+
+  const init = () => {
+    chrome.storage.sync.get(['type', 'options'], function(result) {
+      const { type, options } = result;
+      description.value = getDescription(options);
+    });
+  }
+
+  if (!!description) {
+    init();
+  }
 })();
--- popup.js
+++ popup.js
@@ -9,23 +9,15 @@ addDescription.onclick = function(element) {
     selectedOptions.push(optionsCheckboxes[i].value)
   }
 
-
-  console.log('typeValue', typeValue);
-  console.log('selectedOptions', selectedOptions);
-
   chrome.storage.sync.set({type: typeValue});
   chrome.storage.sync.set({options: selectedOptions});
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    const xxx = '1';
     chrome.tabs.executeScript(
         tabs[0].id,
         {
           file: "description.js",
           allFrames: true
         });
-    // chrome.tabs.executeScript(
-    //     tabs[0].id,
-    //     {code: `document.getElementById('description').value = '${description}';`});
   });
 };
